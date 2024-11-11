3bb906813fe69c6f72ac8eff0fc773426cd5301b
dontwanttorevealname <harrymoxon08@gmail.com>
2024-11-11 13:59:57 +0000

    Full Wordsearch

--- main.py
+++ main.py
@@ -67,6 +67,79 @@ def FillGrid(grid, wordlist):
                 random_int2 = random.randint(0, len(random_word) - 1)
                 letter = random_word[random_int2]
                 grid[i][j] = letter
-    for row in grid:
-        print(row)
+    return grid
 
+def InsertWords(grid, wordlist):
+    if not isinstance(grid, list):
+        print("Error; Input is not a 'list' variable.")
+        return
+    if len(grid) <= 0:
+        print("Error; Grid size too small.")
+        return
+    for item in grid:
+        if not isinstance(item, list):
+            print("Error; Input is not a 'grid' object.")
+            return
+        if len(item) <= 0:
+            print("Error; Grid size too small.")
+            return
+    for word in wordlist:
+        flip = random.randint(0, 1)
+        if flip == 1:
+            grid = InsertWordVertically(grid, word)
+        else:
+            grid = InsertWordHorizontally(grid, word)
+
+    return grid
+
+def InsertWordHorizontally(grid, word):
+    flip = random.randint(0, 4)
+    if flip == 1:
+        word = word[::-1]
+    length = len(word)
+    lengthX = len(grid[0])
+    if length > lengthX:
+        print("Error; Word too big.")
+        return
+    current_row = random.randint(0, len(grid) - 1)
+    start_position = random.randint(0, lengthX - length)
+    for i in range(length):
+        if grid[current_row][start_position + i] != '0':
+            return grid
+    current_position = start_position
+    for letter in word:
+        grid[current_row][current_position] = letter
+        current_position += 1
+        
+    return grid
+
+
+def InsertWordVertically(grid, word):
+    flip = random.randint(0, 4)
+    if flip == 1:
+        word = word[::-1]
+    length = len(word)
+    lengthY = len(grid)
+    if length > lengthY:
+        print("Error; Word too big.")
+        return grid
+    current_column = random.randint(0, len(grid[0]) - 1)
+    start_position = random.randint(0, lengthY - length)
+    for i in range(length):
+        if grid[start_position + i][current_column] != '0':
+            return grid
+        
+    current_position = start_position
+    for letter in word:
+        grid[current_position][current_column] = letter
+        current_position += 1
+        
+    return grid
+
+
+def BuildGrid(wordlist, size, count):
+    grid = MakeGrid(size, size)
+    words = SelectWords(wordlist, count, size)
+    grid = InsertWords(grid, words)
+    grid = FillGrid(grid, words)
+    return grid
--- test.py
+++ test.py
@@ -6,11 +6,20 @@ with open('Words.txt', 'r') as file:
 wordlist= file_contents.split(', ')
 
 
-#words = SelectWords(wordlist, 5)
-grid = MakeGrid(15, 15)
+'''grid = MakeGrid(15, 15)
 print("Empty : \n")
 for row in grid:
     print(row)
+print("\n \n \n Words : \n")
+words = SelectWords(wordlist, 10, 15)
+grid = InsertWords(grid, words)
+for row in grid:
+    print(row)
 print("\n \n \n Filled : \n")
-words = SelectWords(wordlist, 1, 3)
-FillGrid(grid, words)
+grid = FillGrid(grid, words)
+for row in grid:
+    print(row)'''
+
+grid = BuildGrid(wordlist, 10, 15)
+for row in grid:
+    print(row)
